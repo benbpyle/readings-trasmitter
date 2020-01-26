cc5f2d19677ea5c2d235a14bf212d473e2e5b0a7
benbpyle <benbpyle@gmail.com>
2020-01-25 20:05:39 -0600

    Subscription working

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+)
+
 func main() {
-	
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	psc := redis.PubSubConn{Conn: c}
+
+	psc.Subscribe("c1")
+
+	for {
+		switch n := psc.Receive().(type) {
+		case redis.Message:
+			fmt.Printf("Message: %s %s\n", n.Channel, n.Data)
+		case redis.Subscription:
+			fmt.Printf("Subscription: %s %s %d\n", n.Kind, n.Channel, n.Count)
+			if n.Count == 0 {
+				return
+			}
+		case error:
+			fmt.Printf("error: %v\n", n)
+			return
+		}
+	}
 }
